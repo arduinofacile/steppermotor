1e7bf0d1f224c48be8b543174caa40e7eda0263a
arduinofacile <50011535+arduinofacile@users.noreply.github.com>
2019-10-14 09:05:48 +0200

    Add files via upload

---
+++ stepping_motor_driver_a4988.ino
@@ -0,0 +1,45 @@
+/*  Esempio di controllo del Motore stepper
+ *      
+ *  by Michele Aurelio http://www.arduinofacile.it
+ *  
+ */
+
+// definizione dei PIN
+const int stepPin = 3; 
+const int dirPin = 4; 
+const int stepForRevolution = 200;
+const int coeffSpeedLOW = 2000;
+const int coeffSpeedHigh = 1000;
+
+ 
+void setup() {
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+
+
+void loop() {
+  //Motore in direzione oraria
+  digitalWrite(dirPin,HIGH);
+  
+  // Due giri veloci in avanti
+  for(int x = 0; x < 2*stepForRevolution; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(coeffSpeedHigh); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(coeffSpeedHigh); 
+  }
+  delay(1000); // pausa di un secondo
+
+  //Motore in direzione antioraria
+  digitalWrite(dirPin,LOW);
+  
+  // Cinque giri lenti nell'altra direzione 
+  for(int x = 0; x < 5 * stepForRevolution; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(coeffSpeedLOW);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(coeffSpeedLOW);
+  }
+  delay(1000);
+}
